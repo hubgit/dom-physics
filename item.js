62c876c4111b43a4090a8924f8e8b45c21c4b1e3
Alf Eaton <alf@hubmed.org>
2014-01-26 16:09:18 +0000

    minimal

--- item.js
+++ item.js
@@ -1,9 +1,7 @@
 var Item = function(x, y, energy, mass) {
 	this.mass = mass;
 	this.energy = energy;
-	this.delay = 50;
-	this.size = mass;
-	this.interval = null;
+	this.delay = 100;
 	this.velocity = 5;
 	this.vector = [0, 0];
 	this.children = [];
@@ -44,21 +42,6 @@ Item.prototype.run = function() {
 		this[method].apply(this, args);
 	}, this);
 
-	if (this.parent && !this.children.length) {
-		if (Math.abs(this.vector[0] - this.parent.vector[0]) < 0.01 &&
-			Math.abs(this.vector[1] - this.parent.vector[1]) < 0.01) {
-			console.log('confluence');
-			this.die();
-			return;
-		};
-	}
-
-	if (this.parent && !this.children.length && (this.vector[0] < 0.0005 && this.vector[1] < 0.0005)) {
-		console.log('too slow');
-		this.die();
-		return;
-	}
-
 	var item = this;
 	window.setTimeout(function() { item.run() }, this.delay);
 }
@@ -68,23 +51,19 @@ Item.prototype.die = function() {
 
 	parent.energy += this.energy;
 	parent.mass += this.mass;
-	parent.size += this.size;
 
-	//this.parent.velocity[0] -= this.velocity[0];
-	//this.parent.velocity[1] -= this.velocity[1];
 	this.energy = 0;
-	this.size = 0;
+
 	console.log('removing');
 	this.element.parentNode.removeChild(this.element);
 
-	if (parent.children.length) {
-		var guid = this.guid;
-		parent.children.forEach(function(child, index) {
-			if (child.guid == guid) {
-				delete parent.children[index];
-			}
-		});
-	}
+	var guid = this.guid;
+
+	parent.children.forEach(function(child, index) {
+		if (child.guid == guid) {
+			delete parent.children[index];
+		}
+	});
 }
 
 /*
@@ -112,21 +91,6 @@ Item.prototype.sense = function() {
 
 	this.x += this.vector[0];
 	this.y += this.vector[1];
-
-	if (this.parent) {
-		this.vector[0] = (this.vector[0] * 49 + this.parent.vector[0]) / 50;
-		this.vector[1] = (this.vector[1] * 49 + this.parent.vector[1]) / 50;
-	}
-
-	if (this.children.length) {
-		this.vector[0] = (this.children.reduce(function(previousValue, child) {
-			return child.vector[0] + previousValue;
-		}, 0) + this.vector[0] * 4) / (this.children.length + 5);
-
-		this.vector[1] = (this.children.reduce(function(previousValue, child) {
-			return child.vector[1] + previousValue;
-		}, 0) + this.vector[1] * 4) / (this.children.length + 5);
-	}
 }
 
 Item.prototype.move = function() {
@@ -134,7 +98,7 @@ Item.prototype.move = function() {
 	var y = Math.round(this.y);
 
 	this.element.style.webkitTransform = 'translate3d(' + x + 'px, ' + y + 'px, 0) \
-	scale3d(' + this.size + ', ' + this.size + ', 0)';
+	scale3d(' + this.mass + ', ' + this.mass + ', 0)';
 
 	this.element.style.backgroundColor = 'rgba(' + this.color.join(',') + ')';
 
@@ -143,46 +107,27 @@ Item.prototype.move = function() {
 }
 
 Item.prototype.spawn = function() {
-	if (this.energy <= 1) {
-		return;
-	}
-
 	if (Math.random() * 100 > this.mass) {
 		return;
 	}
 
 	console.log('spawn');
 
-	var angle = Math.random() * 360;
-
-	var pieces = Math.round(Math.random() * 5);
-
-	var mass = this.mass / pieces;
-	var energy = mass;
-	var size = this.size / pieces;
-
-	if (energy < 1) {
-		return;
-	}
-
-	for (var i = 0; i < pieces; i++) {
+	while (this.energy > 2) {
 		var item = new Item(this.x, this.y);
 
-		item.mass = mass;
-		this.mass -= mass;
+		item.mass = this.mass / 2;
+		this.mass -= item.mass;
 
-		item.energy = energy;
-		this.energy -= energy;
+		item.energy = this.energy / 2;
+		this.energy -= item.energy;
 
-		item.size = size;
-		this.size -= size;
+		item.velocity = this.velocity;
 
-		angle = Math.random() * 360
+		var angle = Math.random() * 360;
 
 		item.propel(angle);
 		this.propel(-angle);
-
-		console.log(item.mass, item.energy)
 
 		item.parent = this;
 		this.children.push(item);
